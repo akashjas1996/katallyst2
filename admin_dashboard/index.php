905f5fba1042924e1f9627ffb98b797562e66ef0
Akash Jaiswal <akashjas1996@gmail.com>
2019-12-24 00:33:25 +0530

    about added

--- admin_dashboard/index.php
+++ admin_dashboard/index.php
@@ -153,6 +153,27 @@
     </div>
     <!-- Grid row -->
 
+
+        <div class="row">
+      <div class="col-md-6 col-lg-3 mb-4">
+        <div class="card success-color white-text">
+          <div class="card-body d-flex justify-content-between align-items-center">
+            <div>
+              <p class="h2-responsive font-weight-bold mt-n2 mb-0"></p>
+              <p class="mb-0">About Verticals</p>
+            </div>
+            <div>
+              <i class="fas fa-handshake fa-4x text-black-40"></i>
+            </div>
+          </div>
+          <a href="../admin_about/" class="card-footer footer-hover small text-center white-text border-0 p-2">More info<i class="fas fa-arrow-circle-right pl-2"></i></a>
+        </div>
+      </div>
+    </div>
+
+
+
+
   </section>
   <!-- Section: Block Content -->
 
